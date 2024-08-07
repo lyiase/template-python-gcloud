0be063e7bc2e3a571f677a9b94973356d08381a7
Lyiase <6845384+lyiase@users.noreply.github.com>
2024-08-07 04:04:09 +0000

    add basic file set

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+# gcloud install
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install google-cloud-sdk
+
+# python package install
+pip3 install --user -r requirements.txt
